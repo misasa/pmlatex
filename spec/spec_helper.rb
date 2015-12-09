517f8552291a4f8a495a018f818051c1a7329d1b
Yusuke Yachi <yusuke.yachi@gmail.com>
2015-12-09 16:53:22 +0900

    add rcov setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
 require 'pmlatex'
+require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start
+
 Dir.glob("spec/steps/**/*steps.rb"){ |f| load f, true }
 
 class Output
